f61639abd47524613a9e61057d41b4129f7994dd
mdemma <mdemma@student.42nice.fr>
2023-04-28 14:48:13 +0200

    no fix yet

--- Networking/Server.cpp
+++ Networking/Server.cpp
@@ -7,34 +7,46 @@ void Server::_watchLoop() {
 	_socklen = sizeof(_addr);
 
 	while(1) {
-		nev = kevent(_kq, &_evSet, 1, _evList, 32, NULL);
+		nev = kevent(_kq, &_evSet, 1, _evList, 32,  NULL);
+		// for (int i = 0; i < nev; i++)
+		// {
+		// 	std::cout << "TEST NEV pour i = " << i << " FD = " << _evList[i].ident << std::endl;
+		// }
 		if (nev == -1) {
 			perror("kevent() failed");
 			exit(EXIT_FAILURE);
 		}
 		for (int i = 0; i < nev; ++i) {
+			std::cout << "--debut for loop : "<< i << std::endl;
 			if (_evList[i].flags & EV_EOF) //si CTRL+C 
 			{
 				std::cout << "disconnect\n";
 				int fd = _evList[i].ident;
 				EV_SET(&_evSet, fd, EVFILT_READ, EV_DELETE, 0, 0, NULL);
-				// if (kevent(_kq, &_evSet, 1, NULL, 0, NULL) == -1)
-				// 	std::cerr << "kevent: " << strerror(errno) << std::endl;
-				std::cout << "Closing fd 2: " << fd << std::endl;
 				close(fd);
 			}
 			else if ((int)_evList[i].ident == _socket[0]->getFd()) {
 				_accepter(_evList[i].ident);
 			}
 			else {
-				/* TENTATIVE DE FIX QUI FAIT UNE BOUCLE INFINIE SUR LE COUCOU*/
-				// int fd = _evList[i].ident;
-				// int fd_flags = fcntl(fd, F_GETFD);
-				// if (!(fd_flags == -1 && (fd_flags & FD_CLOEXEC)))
-				/* FIN DU FIX*/
-					_handler(_evList[i].ident);
+				fd_set read_fds;
+				FD_ZERO(&read_fds);
+				FD_SET(_evList[i].ident, &read_fds);
+				int num_ready = select(_evList[i].ident + 1, &read_fds, NULL, NULL, NULL);
+				if (num_ready == -1) {
+					std::cout << "ERROR \n";
+					exit(1);
+				}
+				else if (num_ready == 0) {
+					continue ;
+				}
+				else {
+					// At least one socket is ready for reading
+					if (FD_ISSET(_evList[i].ident, &read_fds)) {
+						_handler(_evList[i].ident);
+					}
+				}
 			}
-			// std::cout << "COUCOU\n";
 		}
 	}
 }
@@ -73,6 +85,7 @@ void	Server::_accepter(int server_fd) {
 	}
 	fcntl(this->_requestFd, F_SETFL, O_NONBLOCK);
 	EV_SET(&_evSet, this->_requestFd, EVFILT_READ, EV_ADD, 0, 0, NULL);
+	std::cout << "ACCEPTED = " << _requestFd << std::endl;
 }
 
 // Handle incoming data on accepted connections
@@ -87,20 +100,16 @@ void	Server::_handler(int client_fd) {
 	else if (n == 0) {
 		// Connection closed by client
 		EV_SET(&_evSet, client_fd, EVFILT_READ, EV_DELETE, 0, 0, NULL);
-		// if (kevent(_kq, &_evSet, 1, NULL, 0, NULL) == -1) {
-		// 	perror("kevent() failed");
-		// 	exit(EXIT_FAILURE);
-		// }
 		close(client_fd);
-		std::cout << "Closing fd : " << client_fd << std::endl;
 	}
 	else {
 		// main case: handle received data (print for now)
 		_requestBuffer[n] = '\0';
 		/* PARSE AND CREATE A REQUEST*/
 		printf("Received data from %d: %s", client_fd, _requestBuffer);
-		close(client_fd);
-		std::cout << "Closing 1 fd : " << client_fd << std::endl;
+		EV_SET(&_evSet, client_fd, EVFILT_READ, EV_DELETE, 0, 0, NULL);
+		// close(client_fd); 
+		// std::cout << "Closing 1 fd : " << client_fd << std::endl;
 	}
 }
 
